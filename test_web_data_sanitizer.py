9d9314a8a3f0425b91a233f296da29c6fc3eee02
Ozan Ozgur <ozan1.ozgur@gmail.com>
2017-10-22 23:07:57 +0100

    add a few test stubs

---
+++ test_web_data_sanitizer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+
+import unittest
+
+
+class TestWebDataSanitizer(unittest.TestCase):
+
+    def test_file_exists(self):
+        pass
+
+    def test_file_not_found(self):
+        pass
+
+    def test_csv_reader_object(self):
+        pass
+
+    def test_csv_reader_object_with_no_data_rows(self):
+        pass
+
+    def test_unexpected_number_of_columns_in_csv_file(self):
+        pass
+
+    def test_process_web_data(self):
+        pass
